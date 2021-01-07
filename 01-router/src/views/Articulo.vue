a82611f4ce19212ed67396891a08c6e680f154cd
mario-sanmartin <sanmartin.mario@outlook.com>
2021-01-07 01:03:10 -0300

    ruta con parametros

---
+++ 01-router/src/views/Articulo.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <h1>Art</h1>
+        <titulo texto="Parametro" />
+        <h2>Parametro: {{$route.params.id}}</h2>
+    </div>
+</template>
+
+<script>
+import titulo from '../components/Titulo';
+export default {
+components:{
+    titulo
+}
+}
+</script>
+
+<style>
+
+</style>
