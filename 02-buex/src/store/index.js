8ff0b8f78ec866c2b7a6c7a831898b08f7c50ec4
mario-sanmartin <sanmartin.mario@outlook.com>
2021-01-10 02:07:02 -0300

    Vuexteando heavy

---
+++ 02-buex/src/store/index.js
@@ -0,0 +1,13 @@
+import { createStore } from 'vuex'
+
+export default createStore({
+  state: {
+    contador: 101
+  },
+  mutations: {
+  },
+  actions: {
+  },
+  modules: {
+  }
+})
---
+++ 02-buex/src/views/Home.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <!-- <h1>Mi contador: {{$store.state.contador}}</h1> -->
+    <h1
+      :style="colorContador"
+    >
+     {{titulo}} | Contador desde Vuex: {{contador}}
+    </h1>
+  </div>
+</template>
+
+<script>
+import {mapState} from 'vuex';
+
+export default {
+  name: 'Home',
+  components: {
+  },
+  data() {
+    return {
+      titulo: 'Susano de Itachy'
+    }
+  },
+  // Al mapear el esta siempre ira en el una propiedad Computada
+  computed:{
+    // esto recibe un array porque tendremos multiples state
+    ...mapState(['contador']),
+    // Los 3 puntos son el operador de propagación de js
+    colorContador(){
+     return [this.contador > 100 ? {'color':'green'}:{'color':'red'}]
+    }
+
+  }
+}
+</script>
