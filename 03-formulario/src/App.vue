a78a1c57f2de5ea3c6ac33202b860c8d163c563b
mario-sanmartin <sanmartin.mario@outlook.com>
2021-01-17 23:57:16 -0300

    Guardado de datos en localstarage, haciendo que la informacion no se pierda momentaneamente en el navegador

--- 03-formulario/src/App.vue
+++ 03-formulario/src/App.vue
@@ -1,15 +1,24 @@
 <template>
-  <Navbar/>
-  <div class="container">
-      <router-view/>
-  </div>
+    <div>
+        <Navbar/>
+        <div class="container">
+            <router-view/>
+        </div>
+    </div>
 </template>
 
 <script>
 import Navbar from './components/Navbar';
+import {mapActions} from 'vuex';
 export default {
   components:{
     Navbar
+  },
+  methods:{
+    ...mapActions(['cargarLocalStorage'])
+  },
+  created(){
+    this.cargarLocalStorage()
   }
 }
 </script>
--- 03-formulario/src/store/index.js
+++ 03-formulario/src/store/index.js
@@ -13,29 +13,45 @@ export default createStore({
     }
   },
   mutations: {
+    cargar(state,payload){
+      //el payload seria todo lo que enviamos desde nuestra actions
+        state.tareas = payload
+    },
     set(state,payload){
+      //Crear tarea
       state.tareas.push(payload)
       console.log(state.tareas);
+      const tareas = state.tareas
+      localStorage.setItem('tareas',JSON.stringify(tareas))
     },
     eliminar(state,payload){
       // crear un array nuevo esceptuando el valor eliminado
      state.tareas = state.tareas.filter(item => item.id !== payload)
      console.log('tarea Eliminada');
+
+     const tareas = state.tareas
+     localStorage.setItem('tareas',JSON.stringify(tareas))
     },
     mutTarea(state,payload){
-      //Si no encuentra nada
+      //Si no encuentra nada -- buscar tarea
       if(!state.tareas.find(item => item.id === payload)){
         router.push('/')
         return //para acabar con la ejecucion del codigo
       }
       // Busca algo dentro de nuestro array
       state.tarea = state.tareas.find(item => item.id === payload)
+
+      const tareas = state.tareas
+      localStorage.setItem('tareas',JSON.stringify(tareas))
     },
     mutEditar(state,payload){
       // el payload es nuestra tarea modificada
       state.tareas = state.tareas.map(item => item.id === payload.id ? payload : item )
       console.log('tarea Editada');
       router.push('/')
+
+      const tareas = state.tareas
+      localStorage.setItem('tareas',JSON.stringify(tareas))
     }
   },
   actions: {
@@ -50,6 +66,16 @@ export default createStore({
     },
     editarTarea({commit},tarea){
       commit('mutEditar',tarea)
+    },
+    cargarLocalStorage({commit}){
+      //Si existen carga el array
+      if(localStorage.getItem('tareas')){
+        const tareas = localStorage.getItem('tareas')
+        commit('cargar',JSON.parse(tareas))
+        return
+      }
+      //con esto se crea el array vacio
+      localStorage.setItem('tareas',JSON.stringify([]))
     }
   },
   modules: {
